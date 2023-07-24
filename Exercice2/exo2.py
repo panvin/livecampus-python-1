3aa4aad9acfd4873b39a6194b7428589029a961c
Vincent <panouilleres.vincent@yahoo.fr>
2023-07-24 14:40:00 +0200

    initialisation exo 2

---
+++ Exercice2/exo2.py
@@ -0,0 +1,10 @@
+########################################
+# Exercice 2 Semaine Python Livecampus #
+#        Vincent PANOUILLERES          #
+########################################
+
+def main():
+    print("Hello World")
+    
+if __name__ == "__main__":
+    main()
