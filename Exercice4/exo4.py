20667e9776003ada91b64216497a8298a4f472ad
Vincent <panouilleres.vincent@yahoo.fr>
2023-07-25 12:26:06 +0200

    Début exercice 4

---
+++ Exercice4/exo4.py
@@ -0,0 +1,20 @@
+########################################
+# Exercice 4 Semaine Python Livecampus #
+#        Vincent PANOUILLERES          #
+########################################
+
+
+
+def main():
+    
+    print ("-----Question 1 -------")
+
+    print ("-----Question 2 -------")
+    
+    
+    print ("-----Question 3 -------")
+
+    print ("-----Question 4 -------")
+
+if __name__ == "__main__":
+    main()
