45e0cb7a6b11775e3e527b0a7464177d47c5457a
Vincent <panouilleres.vincent@yahoo.fr>
2023-07-26 10:25:57 +0200

    Début exercice 5

---
+++ Exercice5/exo5.py
@@ -0,0 +1,12 @@
+########################################
+# Exercice 5 Semaine Python Livecampus #
+#        Vincent PANOUILLERES          #
+########################################
+
+
+def main():
+    
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
