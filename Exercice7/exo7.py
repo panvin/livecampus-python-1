54d821f48175859a01f6f28c658631802b845cd8
Vincent <panouilleres.vincent@yahoo.fr>
2023-07-26 16:37:16 +0200

    Début exercice 7

---
+++ Exercice7/exo7.py
@@ -0,0 +1,20 @@
+########################################
+# Exercice 6 Semaine Python Livecampus #
+#        Vincent PANOUILLERES          #
+########################################
+
+import system.infos as infos
+import system.process as process
+
+def main():
+    
+    print ("-----Question 1 -------")
+    
+    print ("-----Question 2 -------")
+
+    print ("-----Question 3 -------")
+
+    print ("-----Question 4 -------")
+
+if __name__ == "__main__":
+    main()
