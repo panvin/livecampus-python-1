a583603a65724f11549374534759e0d8d60b89c7
Vincent <panouilleres.vincent@yahoo.fr>
2023-07-27 15:47:44 +0200

    Début exercice9

---
+++ Exercice9/exo9.py
@@ -0,0 +1,15 @@
+########################################
+# Exercice 9 Semaine Python Livecampus #
+#        Vincent PANOUILLERES          #
+########################################
+
+
+
+def main():
+    
+    print ("-----Question 1 -------")
+   
+    print ("-----Question 2 -------")
+
+if __name__ == "__main__":
+    main()
